684243f96c0c8b0a5ba80513a9446f78cf5e1d94
Liu Yanzhao <eqqlyz@163.com>
2015-04-24 21:38:43 +0800

    Add an iterated version match function

--- src/match.cc
+++ src/match.cc
@@ -11,6 +11,9 @@
 
 static bool match(int *pn, node_id c1[], node_id c2[], State *s);
 
+/*Add by 刘延昭 2015.04.24*/
+static bool iterative_match(int *pn, node_id c1[], node_id c2[], State *s);
+
 static bool match(node_id c1[], node_id c2[], match_visitor vis, 
                  void *usr_data, State *s, int *pcount); 
 
@@ -26,7 +29,9 @@ static bool match(node_id c1[], node_id c2[], match_visitor vis,
  ------------------------------------------------------------*/
 bool match(State *s0, int *pn, node_id c1[], node_id c2[])
   { 
-    return match(pn,c1,c2,s0);
+	  /*Add by 刘延昭 2015.04.24*/
+	  //return iterative_match(pn,c1,c2,s0);
+	  return match(pn,c1,c2,s0);
   }
 
 /*------------------------------------------------------------
@@ -98,6 +103,73 @@ static bool match(int *pn, node_id c1[], node_id c2[], State *s)
     return found;
   }
 
+/*
+ * 迭代式的匹配函数
+*/
+static bool iterative_match(int *pn, node_id c1[], node_id c2[], State *s){
+	// 定义并初始化状态栈，模拟函数调用栈
+	std::stack<State*> stateStack;
+	stateStack.push(s);
+	// 迭代遍历所有状态
+	bool foundMatch = false;
+	while(!(stateStack.empty())){
+		State* curState = stateStack.top();
+		//获取到了一个匹配
+		if(curState->IsGoal()){
+			*pn = curState->CoreLen();
+			curState->GetCoreSet(c1, c2);
+			curState->BackTrack();
+			State* tmp = stateStack.top();
+			stateStack.pop();	//将当前状态弹出，恢复到前一个状态，继续遍历
+			delete tmp;
+			//输出当前匹配
+			foundMatch = true;
+			continue;
+		}
+		//获取到了一个匹配
+		if(curState->IsDead()){
+			curState->BackTrack();
+			State* tmp = stateStack.top();
+			stateStack.pop();	//将当前状态弹出，恢复到前一个状态，继续遍历
+			delete tmp;
+			continue;
+		}
+		node_id n1 = NULL_NODE, n2 = NULL_NODE;
+		bool found=false;
+		bool isContinue = false;
+		while(!found && curState->NextPair(&n1,&n2,n1,n2)){
+			if(curState->IsFeasiblePair(n1,n2)){
+				State *tmpState = curState->Clone();
+				tmpState->AddPair(n1,n2);
+				stateStack.push(tmpState);
+				curState = tmpState;
+				if(curState->IsGoal()){
+					*pn = curState->CoreLen();
+					curState->GetCoreSet(c1, c2);
+					curState->BackTrack();
+					State* tmp = stateStack.top();
+					stateStack.pop();	//将当前状态弹出，恢复到前一个状态，继续遍历
+					delete tmp;
+					foundMatch = true;
+					break;
+				}
+				//获取到了一个匹配
+				if(curState->IsDead()){
+					curState->BackTrack();
+					State* tmp = stateStack.top();
+					stateStack.pop();	//将当前状态弹出，恢复到前一个状态，继续遍历
+					delete tmp;
+					break;
+				}
+				isContinue = true;
+				break;
+			}
+		}
+		if(isContinue) continue;
+		stateStack.pop();
+	}
+	return foundMatch;
+}
 
 
 
